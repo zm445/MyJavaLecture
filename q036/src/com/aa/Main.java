7b863cf71aaaf29172ee3e204110266214a3d070
zm445 <77592061+zm445@users.noreply.github.com>
2022-01-19 17:47:47 +0900

    Add files via upload

---
+++ q036/src/com/aa/Main.java
@@ -0,0 +1,12 @@
+package com.aa;
+
+import com.aa.gym.display.Title;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.println(Title.TITLE);
+		
+		
+	}
+}
---
+++ q036/src/com/aa/gym/display/Title.java
@@ -0,0 +1,8 @@
+package com.aa.gym.display;
+
+public class Title {
+	
+	public static final String TITLE = "고양이헬스장 관리 프로그램";
+	
+	
+}
