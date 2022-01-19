359989ad5263525c8e7841d5951dc2512f0210b7
zm445 <77592061+zm445@users.noreply.github.com>
2022-01-19 17:48:16 +0900

    Add files via upload

---
+++ q035/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Member m = new Member("하늘","010-86","여자");
+			
+		System.out.println(m.getName() + m.getTel() + m.getSex());
+		
+	
+		
+		Member m2 = new Member("문경","010-42","여자");
+		
+		System.out.println(m2.getName() + m2.getTel() + m2.getSex());
+		
+		
+	}
+}
---
+++ q035/src/Member.java
@@ -0,0 +1,27 @@
+
+public class Member {
+	
+	private String name;
+	private String tel;
+	private String sex;
+	
+	public Member(String n, String t, String s) {
+		this.name = n;
+		this.tel = t;
+		this.sex = s;
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+	public String getTel() {
+		
+		return tel;
+	}
+	public String getSex() {
+		
+		return sex;
+	}
+	
+}
